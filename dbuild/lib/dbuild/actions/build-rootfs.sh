af12c559db5bb5213625267c13f4f18ddac20b7a
Tristan Ross <rosscomputerguy@protonmail.com>
2021-02-14 22:04:51 -0800

    Trying to build images

--- dbuild/lib/dbuild/actions/build-rootfs.sh
+++ dbuild/lib/dbuild/actions/build-rootfs.sh
@@ -1,10 +1,52 @@
 #!/usr/bin/env bash
 
+ROOT_PASSWORD="expidus"
+
 while (( "${#DBUILD_ARGS}" )); do
 	case "${DBUILD_ARGS[0]}" in
 		--help|-h)
-			msg "Usage: \033[1mnone\033[00m"
+			msg "Usage: <options...>"
+			msg "Options:"
+			msg "  --package, -p          Adds another package to install, can be used multiple times"
+			msg "  --enable-service, -e   Enables a service, can be used multiple times"
+			msg "  --disable-service, -d  Disables a service, can be used multiple times"
+			msg "  --root-password, -r    Sets the root password, will be encrypted"
+			msg "  --add-user, -a         Adds another user, can be used multiple times"
+			msg "  --hostname, -h         Sets the hostname"
+			msg "  --timezone, -t         Sets the timezone"
 			exit 0
+			;;
+		--package|-p)	
+			PACKAGES+=("${DBUILD_ARGS[1]}")
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		--enable-service|-e)
+			ENABLED_SERVICES+=("${DBUILD_ARGS[1]}")
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		--disable-service|-d)
+			DISABLED_SERVICES+=("${DBUILD_ARGS[1]}")
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		--root-password|-r)
+			ROOT_PASSWORD="$2"
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		--add-user|-a)
+			ADDITIONAL_USERS+=("${DBUILD_ARGS[1]}")
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		--hostname|-h)
+			HOSTNAME="${DBUILD_ARGS[1]}"
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		--timezone|-t)
+			TIMEZONE="${DBUILD_ARGS[1]}"
+			DBUILD_ARGS=("${DBUILD_ARGS[@]:2}")
+			;;
+		*)
+			msg_error "Invalid argument ${DBUILD_ARGS[0]}"
+			exit 1
 			;;
 	esac
 done
@@ -23,9 +65,6 @@ xbps install -R "${XBPS_REPO}" -S >/dev/null
 msg "Installing packages"
 xbps install -R "${XBPS_REPO}" -yu "${PACKAGES[@]}" >/dev/null
 
-KERNEL_SERIES=$(xbps query -S linux | grep pkgver | cut -f2 -d '-' | cut -f1 -d '_')
-KERNEL_VERSION=$(xbps query -S "linux${KERNEL_SERIES}" | grep pkgver | cut -f2 -d '-')
-
 msg "Configuring system"
 sed -i -e "s|HOSTNAME=.*|HOSTNAME=\"${HOSTNAME}\"|g" -e 's|#HOSTNAME|HOSTNAME|g' "${DBUILD_ROOTFS_DIR}/etc/rc.conf"
 msg_debug "Hostname set to ${HOSTNAME}"
@@ -38,6 +77,55 @@ msg_debug "Timezone set to ${TIMEZONE}"
 msg "Reconfiguring packages"
 rootfs_exec "xbps-reconfigure --all --force" >/dev/null
 
+rootfs_adduser() {
+	local p="$1"
+	user_name=$(echo "$p" | cut -f1 -d ' ')
+	user_pword=$(echo "$p" | cut -f2 -d ' ')
+	adduser_opts="${p/${user_name} ${user_pword} /}"
+	if [[ ! -z "${NOT_LIVE}" ]] && [[ "${user_name}" == "expidus" ]]; then
+		msg_warn "Skipping live user"
+	else
+		msg "Adding user ${user_name}"
+		eval useradd "${user_name}" ${adduser_opts} -R "${DBUILD_ROOTFS_DIR}"
+		if [[ "$user_pword" == "-" ]]; then
+			passwd -R "${DBUILD_ROOTFS_DIR}" -d "${user_name}"
+		else
+			echo "${user_name}:${user_pword}" | chpasswd -R "${DBUILD_ROOTFS_DIR}" -c SHA512
+		fi
+	fi
+}
+
+if [[ -z "${SKIP_USERS}" ]]; then
+	msg "Configuring users"
+	while read p; do
+		rootfs_adduser "$p"
+	done < "${DBUILD_LIB_DIR}/users"
+
+	if [[ -f "${HOME}/.config/expidus-sdk/dbuild/users" ]]; then
+		while read p; do
+			rootfs_adduser "$p"
+		done < "${HOME}/.config/expidus-sdk/dbuild/users"
+	fi
+
+	if [[ ! -z "${ADDITIONAL_USERS}" ]]; then
+		for p in ${ADDITIONAL_USERS[@]}; do
+			rootfs_adduser "$p"
+		done
+	fi
+
+	msg "Setting root user password"
+	echo "root:${ROOT_PASSWORD}" | chpasswd -R "${DBUILD_ROOTFS_DIR}" -c SHA512
+
+	if [[ -z "${NOT_LIVE}" ]]; then
+		msg "Configuring live system"
+		sed -i -e 's|autologin-user=.*|autologin-user=expidus|g' -e 's|autologin-session=.*|autologin-session=expidus-shell|g' \
+			-e 's|#autologin-user=|autologin-user=|g' -e 's|#autologin-session=|autologin-session=|g' "${DBUILD_ROOTFS_DIR}/etc/lightdm/lightdm.conf"
+
+		ENABLED_SERVICES+=(lightdm)
+		DISABLED_SERVICES+=(agetty-tty1)
+	fi
+fi
+
 if [[ -z "${SKIP_SERVICES}" ]]; then
 	msg "Configuring system services"
 	enable_service agetty-serial polkitd uuidd "${ENABLED_SERVICES[@]}"
@@ -46,6 +134,11 @@ if [[ -z "${SKIP_SERVICES}" ]]; then
 		disable_service "${DISABLED_SERVICES[@]}"
 		msg_debug "Disabled services: ${DISABLED_SERVICES[@]}"
 	fi
+fi
+
+if type post_build_rootfs >/dev/null 2>&1; then
+	msg "Running post hook"
+	post_build_rootfs
 fi
 
 rootfs_cleanup
